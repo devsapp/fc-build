b16b8f2de5e6f41353de7c2b3cece449f777e774
千风 <qianfeng.zqf@alibaba-inc.com>
2021-08-11 17:56:21 +0800

    feat: upgrade version from 0.0.12 to 0.0.13

--- dist/interface.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isCredentials = void 0;
-function isCredentials(arg) {
-    return arg.AccessKeyID !== undefined;
-}
-exports.isCredentials = isCredentials;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW50ZXJmYWNlLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vc3JjL2ludGVyZmFjZS50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7QUFvRkEsU0FBZ0IsYUFBYSxDQUFDLEdBQVE7SUFDcEMsT0FBTyxHQUFHLENBQUMsV0FBVyxLQUFLLFNBQVMsQ0FBQztBQUN2QyxDQUFDO0FBRkQsc0NBRUMifQ==
--- dist/utils/utils.d.ts
+++ dist/utils/utils.d.ts
@@ -6,5 +6,5 @@ export declare function isCopyCodeBuildRuntime(runtime: string): boolean;
 export declare function checkCommands(useDocker: boolean, runtime: string): void;
 export declare function checkCodeUri(codeUri: string | ICodeUri): string;
 export declare function getArtifactPath({ baseDir, serviceName, functionName }: IBuildDir): string;
-export declare function readLines(fileName: string): Promise<any[]>;
+export declare function readLines(fileName: string): Promise<string[]>;
 export declare function resolveLibPathsFromLdConf(baseDir: string, codeUri: string): Promise<IObject>;
